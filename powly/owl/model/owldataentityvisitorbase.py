b9e9f0b367bcb9a757b03adbb84052d53cfac826
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 16:06:50 +0200

    Added OWLDataEntityVisitorBase

---
+++ powly/owl/model/owldataentityvisitorbase.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from model.owlvisitorbase import OWLVisitorBase
+
+
+class OWLDataEntityVisitorBase(OWLVisitorBase, ABC):
+    @abstractmethod
+    def visit(self, node):
+        """
+        :param node: An object of one of the following classes: OWLDatatype
+        :return:
+        """
+        pass
