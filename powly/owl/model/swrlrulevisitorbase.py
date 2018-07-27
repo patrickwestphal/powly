5b8e99719db7e2babc07743b01b8b35f956ca62a
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 15:52:31 +0200

    Added SWRLRuleVisitorBase

---
+++ powly/owl/model/swrlrulevisitorbase.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod, ABC
+
+from model.owlvisitorbase import OWLVisitorBase
+
+
+class SWRLRuleVisitorBase(OWLVisitorBase, ABC):
+    @abstractmethod
+    def visit(self, node):
+        """
+        :param node: An object of one of the following classes: SWRLRule
+        """
+        pass
---
+++ tests/owl/model/swrlrulevisitorbase_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestSWRLRuleVisitorBase(TestCase):
+    """SWRLRuleVisitorBase test cases"""
