924c47b507798073cec220f56b1e7bafa9514734
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 19:28:22 +0200

    Added OWLPrimitive

---
+++ powly/owl/model/owlprimitive.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod, ABC
+
+from model.owlobject import OWLObject
+
+
+class OWLPrimitive(OWLObject, ABC):
+    """
+    A common interface for: OWLClass, OWLObjectProperty, OWLDataProperty,
+    OWLAnnotationProperty, OWLDatatype, OWLAnonymousIndividual, OWLLiteral, IRI
+    i.e. the basic "leaf" objects that are used in axioms, class expressions and
+    annotations.
+    """
+
+    @abstractmethod
+    def __init__(self, *args):
+        pass
