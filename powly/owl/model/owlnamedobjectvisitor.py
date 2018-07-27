c4d83a0b5a63ec265e594e641f1bb68d6d7d53ac
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 16:28:11 +0200

    Added OWLNamedObjectVisitor

---
+++ powly/owl/model/owlnamedobjectvisitor.py
@@ -0,0 +1,15 @@
+from abc import abstractmethod, ABC
+
+from model.owlentityvisitor import OWLEntityVisitor
+
+
+class OWLNamedObjectVisitor(OWLEntityVisitor, ABC):
+    @abstractmethod
+    def visit(self, entity):
+        """
+        :param entity: Object of one of the following classes: OWLOntology; or
+        of one of these classes: OWLClass, OWLDatatype,
+        OWLNamedIndividual, OWLAnnotationProperty, OWLDataProperty,
+        OWLObjectProperty
+        """
+        pass
