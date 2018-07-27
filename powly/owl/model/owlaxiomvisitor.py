08751fc95ba497dcc246abddadb0f72cf5245622
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 15:56:07 +0200

    Added OWLAxiomVisitor

---
+++ powly/owl/model/owlaxiomvisitor.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+from model.owlannotationaxiomvisitor import OWLAnnotationAxiomVisitor
+from model.owllogicalaxiomvisitor import OWLLogicalAxiomVisitor
+from model.swrlrulevisitorbase import SWRLRuleVisitorBase
+
+
+class OWLAxiomVisitor(
+        OWLAnnotationAxiomVisitor, OWLLogicalAxiomVisitor, SWRLRuleVisitorBase,
+        ABC):
+
+    @abstractmethod
+    def visit(self, axiom):
+        """
+        :param axiom: An object of one of the following classes:
+        OWLDatatypeDefinitionAxiom, OWLDeclarationAxiom; or one of these
+        classes: OWLAnnotationAssertionAxiom, OWLAnnotationPropertyDomainAxiom,
+        OWLAnnotationPropertyRangeAxiom, OWLAsymmetricObjectPropertyAxiom,
+        OWLClassAssertionAxiom, OWLDataPropertyAssertionAxiom,
+        OWLDataPropertyDomainAxiom, OWLDataPropertyRangeAxiom,
+        OWLDifferentIndividualsAxiom, OWLDisjointClassesAxiom,
+        OWLDisjointDataPropertiesAxiom, OWLDisjointObjectPropertiesAxiom,
+        OWLDisjointUnionAxiom, OWLEquivalentClassesAxiom,
+        OWLEquivalentDataPropertiesAxiom, OWLEquivalentObjectPropertiesAxiom,
+        OWLFunctionalDataPropertyAxiom, OWLFunctionalObjectPropertyAxiom,
+        OWLHasKeyAxiom, OWLInverseFunctionalObjectPropertyAxiom,
+        OWLInverseObjectPropertiesAxiom, OWLIrreflexiveObjectPropertyAxiom,
+        OWLNegativeDataPropertyAssertionAxiom,
+        OWLNegativeObjectPropertyAssertionAxiom,
+        OWLObjectPropertyAssertionAxiom, OWLObjectPropertyDomainAxiom,
+        OWLObjectPropertyRangeAxiom, OWLReflexiveObjectPropertyAxiom,
+        OWLSameIndividualAxiom, OWLSubAnnotationPropertyOfAxiom,
+        OWLSubClassOfAxiom, OWLSubDataPropertyOfAxiom,
+        OWLSubObjectPropertyOfAxiom, OWLSubPropertyChainOfAxiom,
+        OWLSymmetricObjectPropertyAxiom, OWLTransitiveObjectPropertyAxiom,
+        SWRLRule
+        """
+        pass
---
+++ tests/owl/model/owlaxiomvisitor_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLAxiomVisitor(TestCase):
+    """OWLAxiomVisitor test cases"""
