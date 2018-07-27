4d5935a5a3c6c74be08150de1a79dbe0aacee68b
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 16:58:35 +0200

    Added OWLLogicalAxiomVisitorEx

---
+++ powly/owl/model/owllogicalaxiomvisitorex.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod, ABC
+
+from model.swrlrulevisitorexbase import SWRLRuleVisitorExBase
+
+
+class OWLLogicalAxiomVisitorEx(SWRLRuleVisitorExBase, ABC):
+    @abstractmethod
+    def visit(self, axiom):
+        """
+        :param axiom: An object of some of the following classes:
+        OWLAsymmetricObjectPropertyAxiom, OWLClassAssertionAxiom,
+        OWLDataPropertyAssertionAxiom, OWLDataPropertyDomainAxiom,
+        OWLDataPropertyRangeAxiom, OWLDifferentIndividualsAxiom,
+        OWLDisjointClassesAxiom, OWLDisjointDataPropertiesAxiom,
+        OWLDisjointObjectPropertiesAxiom, OWLDisjointUnionAxiom,
+        OWLEquivalentClassesAxiom, OWLEquivalentDataPropertiesAxiom,
+        OWLEquivalentObjectPropertiesAxiom, OWLFunctionalDataPropertyAxiom,
+        OWLFunctionalObjectPropertyAxiom, OWLHasKeyAxiom,
+        OWLInverseFunctionalObjectPropertyAxiom,
+        OWLInverseObjectPropertiesAxiom, OWLIrreflexiveObjectPropertyAxiom,
+        OWLNegativeDataPropertyAssertionAxiom,
+        OWLNegativeObjectPropertyAssertionAxiom,
+        OWLObjectPropertyAssertionAxiom, OWLObjectPropertyDomainAxiom,
+        OWLObjectPropertyRangeAxiom, OWLReflexiveObjectPropertyAxiom,
+        OWLSameIndividualAxiom, OWLSubClassOfAxiom, OWLSubDataPropertyOfAxiom,
+        OWLSubObjectPropertyOfAxiom, OWLSubPropertyChainOfAxiom,
+        OWLSymmetricObjectPropertyAxiom, OWLTransitiveObjectPropertyAxiom; or
+        one of these classes: SWRLRule
+        """
+        pass
---
+++ tests/owl/model/owllogicalaxiomvisitorex_tests.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestOWLLogicalAxiomVisitorEx(TestCase):
+    """OWLLogicalAxiomVisitorEx test cases"""
