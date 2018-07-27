27c28537ab235d796cb7f598d9cdec6589980044
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 21:20:00 +0200

    Added OWLNaryClassAxiom

---
+++ powly/owl/model/owlnaryclassaxiom.py
@@ -0,0 +1,81 @@
+from owl.model.hasoperands import HasOperands
+from owl.model.owlclassaxiom import OWLClassAxiom
+from owl.model.owlnaryaxiom import OWLNaryAxiom
+from owl.model.owlsubclassofaxiomsetshortcut import \
+    OWLSubClassOfAxiomSetShortCut
+
+
+class OWLNaryClassAxiom(
+        OWLClassAxiom, OWLNaryAxiom, OWLSubClassOfAxiomSetShortCut,
+        HasOperands):
+
+    def __init__(self, class_expressions, annotations):
+        """
+        :param class_expressions: A collection of OWLClassExpression objects
+        :param annotations: A collection of OWLAnnotation objects
+        """
+        super().__init__(annotations)
+
+        assert class_expressions is not None
+        self.class_expressions = class_expressions
+        self.class_expressions.sort()
+
+    def components(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def components_without_annotations(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def components_annotations_first(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def class_expressions(self):
+        """
+        TODO: Implement
+        Gets all of the top level class expressions that appear in this axiom.
+
+        :return: Sorted stream of class expressions that appear in the axiom.
+        """
+        raise NotImplementedError()
+
+    def operands(self):
+        return self.class_expressions()
+
+    def get_operands_as_list(self):
+        return self.class_expressions
+
+    def contains(self, ce):
+        """
+        Determines if this class axiom contains the specified class expression
+        as an operand.
+
+        :param ce: The OWLClassExpression object to test for
+        :return: Boolean indicating whether the input class expression is
+        contained in this axiom
+        """
+        return ce in self.class_expressions
+
+    def get_class_expressions_minus(self, *ces):
+        """
+        Gets the set of class expressions that appear in this axiom minus the
+        specfied class expressions.
+
+        :param ces: The OWLClassExpression objects to subtract from the class
+        expressions in this axiom
+        :return: A set containing all of the OWLClassExpression objects in this
+        axiom (the class expressions returned by getClassExpressions()) minus
+        the specified list of class expressions
+        """
+        res = self.class_expressions[:]
+
+        for ce in ces:
+            try:
+                while True:
+                    res.remove(ce)
+            except ValueError:
+                pass
+
+        return res
---
+++ tests/owl/model/owlnaryclassaxiom_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLNaryClassAxiom(TestCase):
+    """OWLNaryClassAxiom test cases"""
