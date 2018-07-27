22eb781991e4868c607fb06637f9e78a52a5c36d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 22:01:15 +0200

    Added OWLObjectIntersectionOf

---
+++ powly/owl/model/owlobjectintersectionof.py
@@ -0,0 +1,36 @@
+from powly.owl.model.owlnarybooleanclassexpression import \
+    OWLNaryBooleanClassExpression
+
+
+class OWLObjectIntersectionOf(OWLNaryBooleanClassExpression):
+    """
+    Represents an
+    <a href="http://www.w3.org/TR/owl2-syntax/#Intersection_of_Class_Expressions">ObjectIntersectionOf</a>
+    class expression in the OWL 2 Specification.
+    """
+
+    def __init__(self, operands):
+        """
+        :param operands: A collection/stream of OWLClassExpression objects
+        """
+        super().__init__(operands)
+
+    def as_conjunct_set(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def conjunct_set(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def contains_conjunct(self, ce):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def get_class_expression_type(self):
+        """TODO: Implement"""
+        raise NotImplementedError()
+
+    def accept(self, visitor):
+        """TODO: Implement"""
+        raise NotImplementedError()
---
+++ tests/owl/model/owlobjectintersectionof_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLObjectIntersectionOf(TestCase):
+    """OWLObjectIntersectionOf test cases"""
