9195b1aeb444a80b57a78561689cf8d555221b1e
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 17:25:50 +0200

    Added SWRLObjectVisitorEx

---
+++ powly/owl/model/swrlobjectvisitorex.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod, ABC
+
+from model.swrlrulevisitorexbase import SWRLRuleVisitorExBase
+
+
+class SWRLObjectVisitorEx(SWRLRuleVisitorExBase, ABC):
+    @abstractmethod
+    def visit(self, node):
+        """
+        :param node: An object of one of the following classes: SWRLBuiltInAtom,
+        SWRLClassAtom, SWRLDataPropertyAtom, SWRLDataRangeAtom,
+        SWRLDifferentIndividualsAtom, SWRLIndividualArgument,
+        SWRLLiteralArgument, SWRLObjectPropertyAtom, SWRLSameIndividualAtom,
+        SWRLVariable; or one of these classes: SWRLRule
+        """
+        pass
---
+++ tests/owl/model/swrlobjectvisitorex_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestSWRLObjectVisitorEx(TestCase):
+    """SWRLObjectVisitorEx test cases"""
