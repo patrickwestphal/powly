8d580d6e10f60cd1adbd483dacaafce330317ac2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 19:36:38 +0200

    Added AsOWLNamedIndividual

---
+++ powly/owl/model/asowlnamedindividual.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod, ABC
+
+from exceptions import ClassCastException
+
+
+class AsOWLNamedIndividual(ABC):
+    @abstractmethod
+    def __init__(self, *args):
+        pass
+
+    @staticmethod
+    def is_owl_named_individual():
+        return False
+
+    def as_owl_named_individual(self):
+        if self.is_owl_named_individual():
+            return self
+        else:
+            raise ClassCastException(
+                '%s is not an OWLNamedIndividual' % str(type(self)))
---
+++ tests/owl/model/asowlnamedindividual_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestAsOWLNamedIndividual(TestCase):
+    """AsOWLNamedIndividual test cases"""
