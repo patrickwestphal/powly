dd17bcf055b917e7bfb454e8a3fbaea93934cca0
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-07-27 16:49:30 +0200

    Added OWLAnnotationValueVisitorEx

---
+++ powly/owl/model/owlannotationvaluevisitorex.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod, ABC
+
+from model.owlanonymousindividualvisitorexbase import \
+    OWLAnonymousIndividualVisitorExBase
+from model.owlliteralvisitorexbase import OWLLiteralVisitorExBase
+from model.owlvisitorexbase import OWLVisitorExBase
+
+
+class OWLAnnotationValueVisitorEx(
+        OWLAnonymousIndividualVisitorExBase, OWLLiteralVisitorExBase,
+        OWLVisitorExBase, ABC):
+
+    @abstractmethod
+    def visit(self, entity):
+        """
+        :param entity: An object of one of the following classes: IRI; or of
+        these classes: OWLAnonymousIndividual, OWLLiteral
+        """
+        pass
---
+++ tests/owl/model/owlannotationvaluevisitorex_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLAnnotationValueVisitorEx(TestCase):
+    """OWLAnnotationValueVisitorEx test cases"""
