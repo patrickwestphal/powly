c9469d41d72fa7f0a2f5648d7a6b072188841ed7
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 16:19:47 +0200

    Added OWLDataPropertyRangeAxiom

---
+++ powly/owl/model/owldatapropertyrangeaxiom.py
@@ -0,0 +1,30 @@
+from powly.owl.model.owldatapropertyaxiom import OWLDataPropertyAxiom
+from powly.owl.model.owlpropertyrangeaxiom import OWLPropertyRangeAxiom
+
+
+class OWLDataPropertyRangeAxiom(OWLPropertyRangeAxiom, OWLDataPropertyAxiom):
+    def __init__(self, property, range, annotations=None):
+        """
+        :param property: An OWLDataPropertyExpression objects
+        :param range: An OWLDataRange expression object
+        :param annotations: A collection of OWLAnnotation objects
+        """
+        super().__init__(property, range, annotations)
+
+    def get_axiom_without_annotations(self):
+        raise NotImplementedError()
+
+    def get_annotated_axiom(self, annotations):
+        """
+        :param annotations: A collection/generator of OWLAnnotation objects
+        """
+        raise NotImplementedError()
+
+    def as_owl_sub_class_of_axiom(self):
+        raise NotImplementedError()
+
+    def accept(self, visitor):
+        raise NotImplementedError()
+
+    def get_axiom_type(self):
+        raise NotImplementedError()
---
+++ tests/owl/model/owldatapropertyrangeaxiom_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLDataPropertyRangeAxiom(TestCase):
+    """OWLDataPropertyRangeAxiom test cases"""
