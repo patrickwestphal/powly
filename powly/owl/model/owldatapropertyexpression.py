99ecbe904f68ff94e1aca6f49bc8da48ca263cf3
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 15:26:37 +0200

    Added OWLDataPropertyExpression

---
+++ powly/owl/model/owldatapropertyexpression.py
@@ -0,0 +1,6 @@
+from powly.owl.model.owlpropertyexpression import OWLPropertyExpression
+from powly.owl.model.swrlpredicate import SWRLPredicate
+
+
+class OWLDataPropertyExpression(OWLPropertyExpression, SWRLPredicate):
+    """OWLDataPropertyExpression marker interface"""
---
+++ tests/owl/model/owldatapropertyexpression_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLDataPropertyExpression(TestCase):
+    """OWLDataPropertyExpression test cases"""
