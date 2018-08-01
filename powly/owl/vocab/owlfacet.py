fbd3868abbb2b7d7f915dc3c88ab860baf49cfd1
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 17:00:51 +0200

    Added OWLFacet

---
+++ powly/owl/vocab/owlfacet.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from rdflib.term import URIRef
+
+from owl.vocab.namespaces import Namespaces
+
+
+class OWLFacet(Enum):
+    LENGTH = (Namespaces.XSD, 'length', 'length')
+    MIN_LENGTH = (Namespaces.XSD, 'minLength', 'minLength')
+    MAX_LENGTH = (Namespaces.XSD, 'maxLength', 'maxLength')
+    PATTERN = (Namespaces.XSD, 'pattern', 'pattern')
+    MIN_INCLUSIVE = (Namespaces.XSD, 'minInclusive', '>=')
+    MIN_EXCLUSIVE = (Namespaces.XSD, 'minExclusive', '>')
+    MAX_INCLUSIVE = (Namespaces.XSD, 'maxInclusive', '<=')
+    MAX_EXCLUSIVE = (Namespaces.XSD, 'maxExclusive', '<')
+    TOTAL_DIGITS = (Namespaces.XSD, 'totalDigits', 'totalDigits')
+    FRACTION_DIGITS = (Namespaces.XSD, 'fractionDigits', 'fractionDigits')
+    LANG_RANGE = (Namespaces.RDF, 'langRange', 'langRange')
+
+    def __init__(self, ns, short_form, symbolic_form):
+        self.iri = URIRef(str(ns) + short_form)
+        self.short_form = short_form
+        self.symbolic_form = symbolic_form
+        self.prefixed_name = ns.prefix_name + ':' + short_form
---
+++ tests/owl/vocab/owlfacet_tests.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestOWLFacet(TestCase):
+    """OWLFacet test cases"""
