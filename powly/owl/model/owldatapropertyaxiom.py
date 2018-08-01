689bce4953bbaadb9032991a03a498d381a8d4f2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 15:58:16 +0200

    Added OWLDataPropertyAxiom

---
+++ powly/owl/model/owldatapropertyaxiom.py
@@ -0,0 +1,5 @@
+from powly.owl.model.owllogicalaxiom import OWLLogicalAxiom
+
+
+class OWLDataPropertyAxiom(OWLLogicalAxiom):
+    """Marker interface for data property axioms"""
---
+++ tests/owl/model/owldatapropertyaxiom_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLDataPropertyAxiom(TestCase):
+    """OWLDataPropertyAxiom test cases"""
