e2a5e7efa0ddf6dd6b42ad84e591a708eed515df
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 15:27:40 +0200

    Added OWLDataProperty

---
+++ powly/owl/model/owldataproperty.py
@@ -0,0 +1,7 @@
+from powly.owl.model.owldatapropertyexpression import OWLDataPropertyExpression
+from powly.owl.model.owlobject import OWLObject
+from powly.owl.model.owlproperty import OWLProperty
+
+
+class OWLDataProperty(OWLDataPropertyExpression, OWLProperty, OWLObject):
+    pass
---
+++ tests/owl/model/owldataproperty_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLDataProperty(TestCase):
+    """OWLDataProperty test cases"""
