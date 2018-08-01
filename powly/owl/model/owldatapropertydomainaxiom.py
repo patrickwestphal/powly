491c2135b7b01220e70856eda76b435329345110
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-01 16:04:14 +0200

    Added OWLDataPropertyDomainAxiom

---
+++ powly/owl/model/owldatapropertydomainaxiom.py
@@ -0,0 +1,30 @@
+from powly.owl.model.owldatapropertyaxiom import OWLDataPropertyAxiom
+from powly.owl.model.owlpropertydomainaxiom import OWLPropertyDomainAxiom
+
+
+class OWLDataPropertyDomainAxiom(OWLPropertyDomainAxiom, OWLDataPropertyAxiom):
+    def __init__(self, property, domain, annotations=None):
+        """
+        :param property: An OWLDataPropertyExpression object
+        :param domain: An OWLClassExpression object
+        :param annotations: A collection of OWLAnnotation objects
+        """
+        super().__init__(property, domain, annotations)
+
+    def get_axiom_without_annotations(self):
+        raise NotImplementedError()
+
+    def get_annotated_axiom(self, annotations):
+        """
+        :param annotations: A collection of OWLAnnotation objects
+        """
+        raise NotImplementedError()
+
+    def as_owl_sub_class_of_axiom(self):
+        raise NotImplementedError()
+
+    def accept(self, visitor):
+        raise NotImplementedError()
+
+    def get_axiom_type(self):
+        raise NotImplementedError()
