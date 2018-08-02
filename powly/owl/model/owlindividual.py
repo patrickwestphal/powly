9a94a43c83b0c68f4bdc6b0e55bfcce604f0919a
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-02 14:33:37 +0200

    Added OWLIndividual

---
+++ powly/owl/model/owlindividual.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod
+
+from powly.owl.model.asowlnamedindividual import AsOWLNamedIndividual
+from powly.owl.model.owlobject import OWLObject
+from powly.owl.model.owlpropertyassertionobject import \
+    OWLPropertyAssertionObject
+
+
+class OWLIndividual(OWLObject, OWLPropertyAssertionObject, AsOWLNamedIndividual):
+    """Represents a named or anonymous individual"""
+
+    def is_named(self):
+        """
+        Determines if this individual is an instance of OWLNamedIndividual.
+        Note that this method is the dual of is_anonymous().
+
+        :return: A Boolean indicating whether this individual is an instance
+        of OWLNamedIndividual
+        """
+        return self.is_owl_named_individual()
+
+    @staticmethod
+    def is_individual():
+        return True
+
+    @abstractmethod
+    def as_owl_anonymous_individual(self):
+        """
+        Obtains this individual an anonymous individual if it is indeed
+        anonymous.
+
+        :return: An OWLAnonymousIndividual object,
+        """
+        pass
+
+    @abstractmethod
+    def to_string_id(self):
+        pass
+
+    def accept(self):
+        raise NotImplementedError()
---
+++ tests/owl/model/owlindividual_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLIndividual(TestCase):
+    """OWLIndividual test cases"""
