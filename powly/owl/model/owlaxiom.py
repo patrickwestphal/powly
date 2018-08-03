ce65a8453cb79d8dcc5ee84b6d5be77f75d67fb7
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-03 15:57:02 +0200

    Further implementations in OWLAxiom

--- powly/owl/model/owlaxiom.py
+++ powly/owl/model/owlaxiom.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 from powly.owl.model.hasannotations import HasAnnotations
 from powly.owl.model.owlobject import OWLObject
 
@@ -20,8 +22,16 @@ class OWLAxiom(OWLObject, HasAnnotations):
             self.annotations = annotations
         self.annotations.sort()
 
-    @staticmethod
-    def get_axiom_without_annotations(axiom):
+    def type_index(self):
+        return 2000 + self.get_axiom_type().get_index()
+
+    def annotations(self):
+        return (ann for ann in self.annotations)
+
+    def annotations_as_list(self):
+        return self.annotations
+
+    def get_axiom_without_annotations(self, axiom):
         """
         Gets an axiom that is structurally equivalent to the input axiom without
         annotations. This essentially returns a version of the axiom stripped of
@@ -29,6 +39,65 @@ class OWLAxiom(OWLObject, HasAnnotations):
         :param axiom: OWLAxiom object to divest of annotations
         :return: The annotationless version of the imput axiom
         """
+        # FIXME: axiom could be None, a class type, or an OWLAxiom!
         raise NotImplementedError()
 
-    # ... TODO: Finish
+    @staticmethod
+    def get_annotated_axiom(annotations, axiom):
+        # FIXME: Multiple implementations with differing method signature!!!
+        return axiom.get_annotated_axiom(type(axiom), annotations)
+
+    def is_anonymous(self):
+        return True
+
+    @staticmethod
+    def is_individual():
+        return False
+
+    @staticmethod
+    def is_axiom():
+        return True
+
+    def accept(self, visitor):
+        raise NotImplementedError()
+
+    def equals_ignore_annotations(self, axiom):
+        """
+        :param axiom: An OWLAxiom object
+        """
+        raise NotImplementedError()
+
+    def is_logical_axiom(self):
+        """
+        Determines if this axiom is a logical axiom. Logical axioms are defined
+        to be axioms other than both declaration axioms (including imports
+        declarations) and annotation axioms.
+        """
+        return False
+
+    def is_annotation_axiom(self):
+        """
+        Determines if this axioms in an annotation axiom (an instance of
+        OWLAnnotationAxiom)
+        """
+        return False
+
+    def is_annotated(self):
+        """
+        Determines if this axiom has any annotations on it
+        """
+        return len(self.annotations) != 0
+
+
+    @abstractmethod
+    def get_axiom_type(self):
+        pass
+
+    def is_of_type(self, types):
+        if self.get_axiom_type() in types:
+            return True
+        else:
+            return False
+
+    def get_nnf(self):
+        raise NotImplementedError()
