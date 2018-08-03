219d3e29266209f806feb4a93366c80d58e5e1ef
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-03 16:14:26 +0200

    Clean-ups in OWLNamedObject

--- powly/owl/model/owlnamedobject.py
+++ powly/owl/model/owlnamedobject.py
@@ -1,29 +1,22 @@
 from abc import abstractmethod, ABC
 
+from powly.owl.model.hasiri import HasIRI
 from powly.owl.model.owlnamedobjectvisitor import OWLNamedObjectVisitor
 from powly.owl.model.owlnamedobjectvisitorex import OWLNamedObjectVisitorEx
 from powly.owl.model.owlobject import OWLObject
 
 
-class OWLNamedObject(OWLObject, ABC):
+class OWLNamedObject(OWLObject, HasIRI, ABC):
     """
     Represents a named object for example, class, property, ontology etc. - i.e.
     anything that has an IRI as its name.
     """
 
     @abstractmethod
-    def __init__(self, *args):
-        pass
-
     def accept(self, visitor):
         """
         :param visitor: An object of one of the following classes:
         OWLNamedObjectVisitor, OWLNamedObjectVisitorEx; or one of these classes
         OWLObjectVisitor, OWLObjectVisitorEx
         """
-        if isinstance(visitor, OWLNamedObjectVisitor):
-            visitor.visit(self)
-        elif isinstance(visitor, OWLNamedObjectVisitorEx):
-            return visitor.visit(self)
-        else:
-            return super().accept(visitor)
+        pass
