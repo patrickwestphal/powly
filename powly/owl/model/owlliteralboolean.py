7cfb136925d7ab3e1b30f119db84aa647a9a44be
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-03 15:39:48 +0200

    Added OWLLiteralBoolean

---
+++ powly/owl/model/owlliteralboolean.py
@@ -0,0 +1,31 @@
+from powly.owl.model.owlliteralbase import OWLLiteralBase
+from powly.owl.model.owlobject import OWLObject
+from powly.owl.util.internalizedentities import XSDBOOLEAN
+
+
+class OWLLiteralBoolean(OWLLiteralBase, OWLObject):
+    def __init__(self, literal):
+        assert isinstance(literal, bool)
+        self.literal = literal
+
+    def __hash__(self):
+        hsh = self.hash_index()
+        hsh = OWLObject.hash_iteration(hsh, hash(self.get_datatype()))
+        if self.literal:
+            tmp_hsh = 65536
+        else:
+            tmp_hsh = 0
+        hsh = OWLObject.hash_iteration(hsh, tmp_hsh)
+        return OWLObject.hash_iteration(hsh, hash(self.get_lang()))
+
+    def get_literal(self):
+        return str(self.literal)
+
+    def is_boolean(self):
+        return True
+
+    def parse_boolean(self):
+        return self.literal
+
+    def get_datatype(self):
+        return XSDBOOLEAN
---
+++ tests/owl/model/owlliteralboolean_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLLiteralBoolean(TestCase):
+    """OWLLiteralBoolean test cases"""
