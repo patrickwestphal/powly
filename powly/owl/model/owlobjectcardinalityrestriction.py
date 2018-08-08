0b40a25362c306f0310fc44ab46d9336deff24c9
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-08 17:54:10 +0200

    Added OWLObjectCardinalityRestriction

---
+++ powly/owl/model/owlobjectcardinalityrestriction.py
@@ -0,0 +1,21 @@
+from powly.owl.model.owlcardinalityrestriction import OWLCardinalityRestriction
+from powly.owl.model.owlobjectrestriction import OWLObjectRestriction
+from powly.owl.model.owlquantifiedobjectrestriction import \
+    OWLQuantifiedObjectRestriction
+
+
+class OWLObjectCardinalityRestriction(
+        OWLCardinalityRestriction, OWLQuantifiedObjectRestriction,
+        OWLObjectRestriction):
+    def __init__(self, property, cardinality, filler):
+        """
+        :param property: An OWLObjectPropertyExpression object
+        :param cardinality: An integer object
+        :param filler: An OWLClassExpression object
+        """
+        super().__init__(cardinality, filler)
+        assert property is not None
+        self.property = property
+
+    def get_property(self):
+        return self.property
---
+++ tests/owl/model/owlobjectcardinalityrestriction_tests.py
@@ -0,0 +1,5 @@
+from unittest.case import TestCase
+
+
+class TestOWLObjectCardinalityRestriction(TestCase):
+    """OWLObjectCardinalityRestriction test cases"""
