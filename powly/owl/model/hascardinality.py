54d38bfe29de6eb4e674d299a8d501b2025b039e
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-08-08 17:48:26 +0200

    Minor updates in HasCardinality

--- powly/owl/model/hascardinality.py
+++ powly/owl/model/hascardinality.py
@@ -4,4 +4,7 @@ from abc import ABC, abstractmethod
 class HasCardinality(ABC):
     @abstractmethod
     def get_cardinality(self):
+        """
+        :return: An integer object
+        """
         pass
